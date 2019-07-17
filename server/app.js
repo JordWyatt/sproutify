19d8a907ea52008f8bfff601ed5eca4970903fb4
jwyatt1995@gmail.com <jwyatt1995@gmail.com>
2019-07-17 23:45:25 +0100

    Reorg

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
