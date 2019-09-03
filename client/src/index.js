26650c895b16145e6e568611be42a8388a28248b
jwyatt1995@gmail.com <jwyatt1995@gmail.com>
2019-09-03 22:35:04 +0100

    Add dotenv, update .gitignore

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 render(
   <BrowserRouter>
